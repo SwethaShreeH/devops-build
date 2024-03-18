424c5da4a60a80c0ccec6a98139d00aea6437692
SwethaShreeH <63458930+SwethaShreeH@users.noreply.github.com>
2024-03-18 19:37:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 
 docker-compose build
 
-docker tag default_my-react-app:latest swetha1003/prod:latest
-docker push swetha1003/prod:latest
+docker tag default_my-react-app:latest swetha1003/dev:latest
+docker push swetha1003/dev:latest
